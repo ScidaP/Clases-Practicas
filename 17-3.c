f0769268a51c13e012978df29efe3c2b58baae5f
Patricio Scidá <patricioscida@gmail.com>
2022-03-19 16:09:05 -0300

    avanzando

--- 17-3.c
+++ 17-3.c
@@ -27,7 +27,7 @@ Nodo * CrearNodo(int X) { // Funciona
     return nuevoNodo;
 }
 
-Nodo * insertar(Nodo * Lista) {
+Nodo * insertar(Nodo * Lista) { // Funciona
     int numeroRandom = rand() % 100 + 1; //Numero random entre 1 y 100
     Nodo * NuevoNodo = CrearNodo(numeroRandom);
     NuevoNodo->siguiente = Lista;
@@ -50,7 +50,48 @@ Nodo * borrar(Nodo * Lista) { // Funciona
         free(aux);
         return Lista;
     }
+}
 
+int longitud(Nodo * Lista) { // Funciona
+    if (Lista == NULL) {
+        return 0;
+    } else {
+        int i = 0;
+        while (Lista != NULL) {
+            i++;
+            Lista = Lista->siguiente;
+        }
+        return i;
+    }
+}
+
+int pertenece(Nodo * Lista, int dato) { // Funciona
+    if (Lista == NULL) {
+        return 0;
+    } else {
+        while (Lista != NULL) {
+            if (Lista->dato == dato) {
+                return 1;
+                break;
+            } else {
+                Lista = Lista->siguiente;
+            }
+        }
+    }
+}
+
+Nodo * borrarK(Nodo * Lista, int datoABorrar) { // En desarrollo
+    if (Lista == NULL) {
+        return Lista;
+    } else {
+        Nodo * aux = CrearNodo(0);
+        aux = Lista;
+        while (Lista != NULL) {
+            if (Lista->dato == datoABorrar) {
+                Nodo * NuevaLista = borrar(Lista);
+            }
+        }
+    }
 }
 
 int main() {
@@ -62,7 +103,11 @@ int main() {
     ListaNueva = insertar(ListaNueva);
     ListaNueva = insertar(ListaNueva);
     primerElemento(ListaNueva);
-    ListaNueva = borrar(ListaNueva);
+    int longitudLista = longitud(ListaNueva);
+    printf("Longitud de la lista: %d\n", longitudLista);
+    int valorAEncontrar = 8;
+    int valorEncontrado = pertenece(ListaNueva, valorAEncontrar);
+    printf("Resultado de la coincidencia de %d en la Lista: %d (1=Encontrado|0=No Encontrado)", valorAEncontrar, valorEncontrado);
     getchar();getchar();getchar();getchar();
     return 0;
 }
