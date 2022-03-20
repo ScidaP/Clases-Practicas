14d2c0f65a523337d0d5489bbeb876a2d96dd9c6
Patricio Scidá <patricioscida@gmail.com>
2022-03-20 15:33:16 -0300

    desarrollando estaContenida()

--- 17-3.c
+++ 17-3.c
@@ -28,7 +28,7 @@ Nodo * CrearNodo(int X) { // Funciona
 }
 
 Nodo * insertar(Nodo * Lista) { // Funciona
-    int numeroRandom = rand() % 100 + 1; //Numero random entre 1 y 100
+    int numeroRandom = rand() % 15 + 1; //Numero random entre 1 y 100
     Nodo * NuevoNodo = CrearNodo(numeroRandom);
     NuevoNodo->siguiente = Lista;
     Lista = NuevoNodo;
@@ -94,6 +94,32 @@ Nodo * borrarK(Nodo * Lista, int datoABorrar) { // En desarrollo
     }
 }
 
+int estaContenida(Nodo * Lista1, Nodo * Lista2) { // En desarrollo
+    int coincidencias = 0;
+    Nodo * aux = CrearNodo(0);
+    aux = Lista1;
+    int longitudLista1 = longitud(Lista1);
+    int longitudLista2 = longitud(Lista2);
+    for (int i = 0; i < longitudLista1; i++) {
+        Nodo * aux2 = CrearNodo(0);
+        aux2 = Lista2;
+        for (int k = 0; k < longitudLista2; k++) {
+            if (aux->dato == aux2->dato) {
+                coincidencias++;
+                aux = borrar(aux); //Si el primer nodo coincide, entonces ya no me sirve y lo borro. De esta manera, en la proxima iteración, se comparará con el segundo y así sucesivamente.
+            } else {
+                aux2 = borrar(aux2); //Si el primer nodo de la primera lista no coincide, entonces borro el priemr nodo de la segunda lista, de esta manera se comparará el primer nodo con el segundo de la segunda lista y así.
+            }
+        }
+    }
+    //RETURN
+    if (longitudLista1 == coincidencias) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main() {
     srand(time(NULL));
     Nodo * ListaNueva = (Nodo*)malloc(sizeof(ListaNueva));
@@ -101,13 +127,38 @@ int main() {
     ListaNueva = insertar(ListaNueva);
     ListaNueva = insertar(ListaNueva);
     ListaNueva = insertar(ListaNueva);
-    ListaNueva = insertar(ListaNueva);
-    primerElemento(ListaNueva);
+    /* primerElemento(ListaNueva);
     int longitudLista = longitud(ListaNueva);
     printf("Longitud de la lista: %d\n", longitudLista);
     int valorAEncontrar = 8;
     int valorEncontrado = pertenece(ListaNueva, valorAEncontrar);
-    printf("Resultado de la coincidencia de %d en la Lista: %d (1=Encontrado|0=No Encontrado)", valorAEncontrar, valorEncontrado);
+    printf("Resultado de la coincidencia de %d en la Lista: %d (1=Encontrado|0=No Encontrado)", valorAEncontrar, valorEncontrado); */
+    Nodo * ListaNueva2 = (Nodo*)malloc(sizeof(ListaNueva2));
+    ListaNueva2 = crearListaVacia();
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    ListaNueva2 = insertar(ListaNueva2);
+    int esContenida = estaContenida(ListaNueva, ListaNueva2);
+    printf("[0 = No contenida | 1 = Esta contenida]. Resultado: %d", esContenida);
     getchar();getchar();getchar();getchar();
     return 0;
 }
